d4b05782c758ed482facbf6132a70be122228e16
kekoawong <kwong6@nd.edu>
2021-09-26 12:39:39 -0400

    Stylizstic improvements

--- renderjs/education.js
+++ renderjs/education.js
@@ -6,7 +6,7 @@ let DO = {
     "link": "https://sites.google.com/puhsd.k12.ca.us/delorohigh/home",
     "logo": "img/del_oro.jpeg",
     "location": "Loomis, California, USA",
-    "listItems": ["Graduated Magna Cum Laudee and honored with Del Oro Parent's Club Scholarship.",
+    "listItems": ["Graduated Magna Cum Laudee and honored with scholarships from the Del Oro Parents' Club and the Society of American Military Engineers.",
                     "Awarded Math Department Award for exhibiting an extended curiosity and in-depth understanding of mathematics.",
                     "3-year varsity athlete in both basketball and tennis."
                 ]
