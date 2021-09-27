1e6265fd55096ab938cf79dc9c776acdf0897a2a
kekoawong <kwong6@nd.edu>
2021-09-27 19:07:00 -0400

    Added education file

--- renderjs/education.js
+++ renderjs/education.js
@@ -1,17 +1,21 @@
 import { container } from "./container.js";
 
-let DO = {
-    "heading": "Del Oro High School",
-    "subheading": "2014 - 2018 | GPA: 4.31",
-    "link": "https://sites.google.com/puhsd.k12.ca.us/delorohigh/home",
-    "logo": "img/del_oro.jpeg",
-    "location": "Loomis, California, USA",
-    "listItems": ["Graduated Magna Cum Laudee and honored with a class rank of 5/429.",
-                    "Awarded Math Department Award for exhibiting an extended curiosity and in-depth understanding of mathematics.",
-                    "3-year varsity athlete in both basketball and tennis."
-                ],
-    "bottomInfo": {"Honors and Awards:": "California Scholarship Federation, National Honors Society, Del Oro Parents' Club Scholarship, Society of American Military Engineers Scholarship."}
-}
+let education = [
+    {
+        "heading": "Del Oro High School",
+        "subheading": "2014 - 2018 | GPA: 4.31",
+        "link": "https://sites.google.com/puhsd.k12.ca.us/delorohigh/home",
+        "logo": "img/del_oro.jpeg",
+        "location": "Loomis, California, USA",
+        "listItems": ["Graduated Magna Cum Laudee and honored with a class rank of 5/429.",
+                        "Awarded Math Department Award for exhibiting an extended curiosity and in-depth understanding of mathematics.",
+                        "3-year varsity athlete in both basketball and tennis."
+                    ],
+        "bottomInfo": {"Honors and Awards:": "California Scholarship Federation, National Honors Society, Del Oro Parents' Club Scholarship, Society of American Military Engineers Scholarship."}
+    } 
+];
 
-let section = document.querySelector('#education');
-section.innerHTML += container(DO);
+education.forEach((e) => {
+    let section = document.querySelector('#education');
+    section.innerHTML += container(e);
+});
---
+++ renderjs/experience.js
@@ -0,0 +1,8 @@
+import { container } from "./container.js";
+
+let experiences = [
+    
+];
+
+let section = document.querySelector('#experience');
+section.innerHTML += container(DO);
