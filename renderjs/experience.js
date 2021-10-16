abd92de5836a36c1b9cad30df74d0e0060e70d35
kekoawong <kwong6@nd.edu>
2021-10-16 17:24:05 -0500

    Added experience sections

--- renderjs/experience.js
+++ renderjs/experience.js
@@ -20,7 +20,52 @@ let experience = [
                     "Facebook": "https://www.facebook.com/eventurely.app.1",
                     "Instagram": "https://www.instagram.com/_eventurely_/"
                     }
-    } 
+    },
+    {
+        "heading": "Jr. Software Engineer in Startup Business Intelligence",
+        "location": "Virtual",
+        "subheading": "The Idea Center",
+        "link": "https://ideacenter.nd.edu/",
+        "logo": "img/idea_center.jpg",
+        "listItems": ["Designed, implemented, and tested listings and product details page for innovation lab’s online marketplace.",
+                        "Conducted extensive research into compatible marketplace repositories to use as a foundation for the project and inspected documentation and code while presenting findings to the project manager."
+                    ],
+        "bottomInfo": {"Tech:": "ReactJS, JavaScript, HTML, CSS, Bootstrap, Yarn, NodeJS, Sharetribe FLEX"},
+        "buttons": {"Repository": "https://github.com/jaeyoungchang5/idea-marketplace",
+                    "Website": "https://ideacenter.nd.edu/"
+                    }
+    },
+    {
+        "heading": "Social Media Misinformation Research",
+        "location": "Notre Dame, Indiana, USA",
+        "subheading": "Dr. Tim Wenginer | University of Notre Dame",
+        "link": "https://timweninger.com/",
+        "logo": "img/nd_logo.png",
+        "listItems": ["Self-taught and carefully learned to write scalable code for large data sets that would operate efficiently and safely with memory on shared resources, creating models of topic activity scraped from online traffic.",
+                        "Authorized to conduct multi-core computational programs with Notre Dame’s Center for Research Computing on machines shared with professors and PhD students."
+                    ],
+        "bottomInfo": {"Tech:": "Python, Parallel Computing, Data Science, Pickle, JSON, Git, Bash, Parsing, Machine Learning"},
+        "buttons": {"Repository": "https://github.com/kekoawong/wikipedia_parsers",
+                    "Professor's Website": "https://timweninger.com/"
+                    }
+    },
+    {
+        "heading": "Data Visualization Toolkit Developer",
+        "location": "Notre Dame, Indiana, USA",
+        "subheading": "Dr. Chaoli Wang | University of Notre Dame",
+        "link": "https://sites.nd.edu/chaoli-wang/",
+        "logo": "img/nd_logo.png",
+        "listItems": ["Embraced a fast-paced environment and learning on the go, self-teaching technical skills and researching solutions in order to ship well-written and efficient software while fulfilling deadlines.",
+                        "Transformed the entire code base into a simplified restructured format which greatly improved the product testing process while also alleviating the code complexity overhead for all team members.",
+                        "Published as a contributor to the educational software toolkit, which is currently being used in the curriculum of an upper-level undergraduate/graduate course on data visualization at the University of Notre Dame."
+                    ],
+        "bottomInfo": {"Tech:": "JavaScript, D3.js, HTML, CSS, Git, Bash, JSON, Data Visualizations, Graph Networks"},
+        "buttons": {"GraphVisual": "https://www3.nd.edu/~cwang11/graphvisual/",
+                    "Publication": "https://www3.nd.edu/~cwang11/research/asee20-graphvisual.pdf",
+                    "Repository": "https://github.com/mimre25/graph-visual",
+                    "Professor's Website": "https://sites.nd.edu/chaoli-wang/"
+                    }
+    },
 ];
 
 experience.forEach((e) => {
