35ad8e14f60036f40fc442e559bb4bb14ea84d15
kekoawong <kwong6@nd.edu>
2021-12-21 18:57:01 -0800

    Added more projects

--- renderjs/projects.js
+++ renderjs/projects.js
@@ -16,11 +16,25 @@ let projects = [
         "file": "pdf/Us_and_AI.pdf"
     },
     {
+        "title": "Mobile Boilerplate",
+        "explanation": "Developed a boilerplate mobile frontend with React Native and libraries such as React Navigation.",
+        "tags": ["sd", "rn"],
+        "date": "July 2021",
+        "github": "https://github.com/kekoawong/boilerplate",
+    },
+    {
         "title": "CampusWayzz",
-        "explanation": "Designed and developed a mobile app using a MERN stack (MongoDB, Express.js, React Native, Node.js) for college campus discovery. Utilized frontend libraries such as React Navigation and React Native Paper to create a strong UI.",
+        "explanation": "Designed and developed a mobile app using a MERN stack (MongoDB, Express.js, React Native, Node.js) for college campus discovery. Utilized frontend libraries such as React Navigation and React Native Paper for a well-implemented and consistent UI.",
         "tags": ["sd", "rn"],
         "date": "April 2021",
         "github": "https://github.com/kekoawong/campuswayzz"
+    },
+    {
+        "title": "Personal Website",
+        "explanation": "Built this personal website using JavaScript, CSS, and HTML. Hosted at kekoawong.com",
+        "tags": ["sd"],
+        "date": "In Progress",
+        "github": "https://github.com/kekoawong/personal_website"
     },
     {
         "title": "Political Tweet Classifier",
@@ -30,6 +44,20 @@ let projects = [
         "file": "pdf/TweetMLClassification.pdf",
         "github": "https://github.com/kekoawong/tweet_classification"
     },
+    {
+        "title": "Idea Center Marketplace",
+        "explanation": "Prototyped marketplace for innovation lab products, using Sharetribe Flex and ReactJS.",
+        "tags": ["sd", "rn"],
+        "date": "August 2020",
+        "github": "https://github.com/kekoawong/idea-marketplace"
+    },
+    {
+        "title": "Wikipedia Parsers",
+        "explanation": "Developed reusable and efficient scripts to parse large amounts of wikipedia traffic data, organizing data into compatible structures for inference scripts.",
+        "tags": ["python"],
+        "date": "April 2020",
+        "github": "https://github.com/kekoawong/wikipedia_parsers"
+    }
 ];
 
 projects.forEach((p) => {
